12f7a28521178f23e42032bc8f98aa7cabd958a6
Ali akbra Saleh <salehbigdeli@email.kntu.ac.ir>
2019-02-07 17:32:25 +0330

    now we can add tree nums.

--- tests/test_calc.py
+++ tests/test_calc.py
@@ -6,3 +6,8 @@ def test_add_two_numbers():
     c = Calc()
     res = c.add(4, 5)
     assert res == 9
+
+def test_adding_multiple_numbers():
+    c = Calc()
+    res = c.add(1, 2, 3)
+    assert res == 6
